61874b5bbbd959c079def1e9a61a53aa7bb296c3
bitnip <sydneyleannephillips@gmail.com>
2025-05-03 19:03:12 -0600

    Fix linux build.

--- cfg/cfg.mk
+++ cfg/cfg.mk
@@ -5,3 +5,14 @@ TEST_SOURCE= \
 	src/wavefront_object_parser_test.c
 LIBRARIES=-lcutil -L ../cutil/bin
 INCLUDES=-I../
+
+COVERAGE_CC=gcc
+ifeq ($(shell uname -s),Darwin)
+	CC=gcc
+endif
+ifeq ($(shell uname -s),Linux)
+	CC=gcc
+endif
+ifeq ($(OS),Windows_NT)
+	CC=x86_64-w64-mingw32-gcc
+endif
